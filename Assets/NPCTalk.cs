82ea237916346cdc4badf529d3be82e0a484c21c
Zachary <zacharytjones1@gmail.com>
2022-06-11 14:17:13 -0700

    We can now talk to multiple people.

---
+++ Assets/NPCTalk.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCTalk : MonoBehaviour
+{
+    public string startNode = "Start";
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
