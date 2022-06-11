69b69905459405806234b77523bf3bf57a60d588
Zachary <zacharytjones1@gmail.com>
2022-06-11 11:27:19 -0700

    We can talk to uncle al now

--- Assets/PlayerTalk.cs
+++ Assets/PlayerTalk.cs
@@ -7,6 +7,8 @@ public class PlayerTalk : MonoBehaviour
 {
     public GameObject dialogueHolder;
     DialogueRunner dialogueRunner;
+    [SerializeField] private bool canTalk = false;
+    [SerializeField] private string targetNode = "Start";
 
     private void Start() 
     {
@@ -18,5 +20,19 @@ public class PlayerTalk : MonoBehaviour
         Debug.Log("You've tried to talk! ");
         Debug.Log(dialogueRunner.Dialogue.CurrentNode);
         Debug.Log(dialogueRunner.startNode);
+        // I'll need to run the dialogue from here.
+        // dialogueRunner.Dialogue.SetNode("Start"); // This SHOULD start at.. start
+        // Okay, so we can set the node, how do we kick start this sucker?
+        if(canTalk)
+        {
+            dialogueRunner.StartDialogue(targetNode);
+        }
+    }
+
+    public void TalkRange(bool inRange)
+    {
+        // This will fire when we enter or leave talk range.
+        if(inRange) canTalk = true;
+        else canTalk = false;
     }
 }
